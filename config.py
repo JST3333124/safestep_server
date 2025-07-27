23b02c2d4c68a60853804c1277da93bf9c7a1683
JST3333124 <snap32320@gmail.com>
2025-07-28 06:31:01 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
-import json
 
+# Firebase 인증 키 파일 경로
 FCM_CREDENTIALS = "firebase-adminsdk.json"
+
+# 데이터베이스 URL (환경변수가 없으면 SQLite 기본 파일로)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+# JWT용 시크릿 키 (없으면 디폴트로 설정)
+SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
