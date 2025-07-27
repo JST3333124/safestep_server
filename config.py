57100abf20187739634ec1e9a11bce623e16ef21
JST3333124 <snap32320@gmail.com>
2025-07-28 06:28:10 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-import firebase_admin
-from firebase_admin import credentials
+import os
+import json
 
-cred = credentials.Certificate("firebase-adminsdk.json")
-
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
-
-FCM_CREDENTIALS = cred
+FCM_CREDENTIALS = "firebase-adminsdk.json"
