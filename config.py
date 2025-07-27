87b29ebb8193ccaab2005c485056c5e6b9d98cdd
JST3333124 <snap32320@gmail.com>
2025-07-28 06:21:25 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-import os
-import json
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate(json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"]))
+cred = credentials.Certificate("firebase-adminsdk.json")
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
